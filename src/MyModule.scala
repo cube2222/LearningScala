4d4d120dff6a513412e6f0893fb3d6dfd420d24c
Jakub Martin <jakub.wit.martin@gmail.com>
2016-01-02 22:37:11 +0100

    aaa

--- src/MyModule.scala
+++ src/MyModule.scala
@@ -6,12 +6,40 @@ object MyModule {
     if (n < 0) -n
     else n
 
-  private def formatAbs(x: Int) = {
-    val msg = "The absolute value of %d is %d"
-    msg.format(x, abs(x))
+  def factorial(n: Int): Int = {
+    def go(n: Int, acc: Int): Int = {
+      if (n <= 0) acc
+      else go(n-1, n*acc)
+    }
+    go(n,1)
   }
 
+  def formatFunction(f: Int => Int, n: Int): String = {
+    val msg = "The function of %d is %d"
+    msg.format(n, f(n))
+  }
+
+  def curry[A,B,C](f: (A,B) => C): A => (B => C) = {
+    def tempFunction(a: A): (B => C) = {
+      def midTempFunction(b: B): C = {
+        f(a,b)
+      }
+      midTempFunction
+    }
+    tempFunction
+  }
+
+  def Fib(n: Int): Int = {
+    @annotation.tailrec
+    def go(n1: Int, n2: Int, RepsLeft: Int): Int = {
+      if (RepsLeft <= 0) n2
+      else go(n2, n1+n2, RepsLeft - 1)
+    }
+    if(n == 1) 0
+    else if (n == 2) 1
+    else go(0,1,n-2)
+  }
   def main(args: Array[String]): Unit = {
-    println(formatAbs(-42))
+
   }
 }
