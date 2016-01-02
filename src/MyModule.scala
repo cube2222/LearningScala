09d4c621258f7381f96843d4be9301b108963e56
Jakub Martin <jakub.wit.martin@gmail.com>
2016-01-02 15:23:40 +0100

    Initial commit

---
+++ src/MyModule.scala
@@ -0,0 +1,17 @@
+/**
+  * Created by jakub on 1/2/2016.
+  */
+object MyModule {
+  def abs(n: Int): Int =
+    if (n < 0) -n
+    else n
+
+  private def formatAbs(x: Int) = {
+    val msg = "The absolute value of %d is %d"
+    msg.format(x, abs(x))
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(formatAbs(-42))
+  }
+}
